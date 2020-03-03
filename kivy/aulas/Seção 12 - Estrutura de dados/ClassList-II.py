4f34fc504d5aa307cc9e252a45a5bc2e1e11d466
Pedro Ramos <ramosct70@hotmail.com>
2020-03-03 15:34:48 -0300

    Atualização

---
+++ kivy/aulas/Seção 12 - Estrutura de dados/ClassList-II.py
@@ -0,0 +1,15 @@
+# Formas de definir uma lista
+lista1 = [1, 2, 3]
+lista2 = ['a', 'b', 'c']
+lista3 = ['a', 2, 'c']
+
+# lista que recebe listas
+lista = [lista1, lista2, lista3]
+print(lista)
+
+# Acessar um item de uma lista dentro de outra lista
+print(lista[1][1])
+
+# Criar uma lista com vários elementos iguais
+l = 10*[0]
+print(l)
