b694210a4dcb17da68e731e0f6407db63b41f3b6
Pedro Ramos <ramosct70@hotmail.com>
2020-03-05 21:42:11 -0300

    Atualização

---
+++ kivy/aulas/Seção 12 - Estrutura de dados/FatiandoListas.py
@@ -0,0 +1,11 @@
+lista = "Bem-vindo ao Curso de python"
+# para fatiar listas nós usamos
+# lista[start:stop:step]
+# start: onde começamos
+# stop: onde paramos
+# step: pulo de item em item
+
+print(lista[:20]) # Pega todas as letras até 20
+print(lista[::2]) # Pega todas as letras pulando uma de cada vez
+print(lista[::-1]) # Mostra a string invertida
+print(lista[22:]) # Pega todas as letras a partir do 22
