51dc11c47a1abd7cc10d640a6ba344fbc09c1b3f
Pedro Ramos <ramosct70@hotmail.com>
2020-03-18 09:28:48 -0300

    Atualização

---
+++ kivy/aulas/Seção 12 - Estrutura de dados/OrdenandoListas.py
@@ -0,0 +1,13 @@
+lista = ['Pedro', 'Eduarda', 'Geovani', 'Vinicius']
+
+# Inverter
+lista.reverse()
+print(lista)
+
+# Ordem alfabética
+lista.sort()
+print(lista)
+
+# Ordem alfabética ao contrário
+lista.sort(reverse=True)
+print(lista)
