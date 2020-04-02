3c6c4555dcce856d418fd5ef0397f73b44905f9b
Pedro Ramos <ramosct70@hotmail.com>
2020-04-01 21:13:27 -0300

    Atualização

---
+++ kivy/aulas/Seção 15 - Funções/NonLocal.py
@@ -0,0 +1,24 @@
+var_local = 0
+def func_non_local():
+    var_local = 10
+
+    def func_interna():
+        nonlocal var_local
+        var_local += 1
+        print(var_local)
+
+    func_interna()
+
+
+def func_global():
+    def func_interna():
+        global var_local
+        var_local += 1
+        print(var_local)
+
+    func_interna()
+
+
+
+func_non_local()
+func_global()
