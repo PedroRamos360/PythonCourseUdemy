e26c9203cd7dc9aedd665ad7c27fff89908905bb
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-01 15:35:10 -0300

    Atualização

---
+++ kivy/aulas/Seção 18 - Orientação a objeto/Personalizar construtores.py
@@ -0,0 +1,11 @@
+class Retangulo:
+    def __init__(self, largura, altura):
+        self.l = largura
+        self.a = altura
+
+    def area(self):
+        return self.l * self.a
+
+
+r = Retangulo(7, 5)
+print(r.area())
