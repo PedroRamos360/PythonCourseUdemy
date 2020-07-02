6dbc932c22a29c6290645d6e145f132f0e4dd0da
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-02 20:54:19 -0300

    Atualização

--- kivy/aulas/Seção 19 - Orientação a Objeto II/MetodosAssessores.py
+++ kivy/aulas/Seção 19 - Orientação a Objeto II/MetodosAssessores.py
@@ -1,7 +1,12 @@
 class Retangulo:
     def __init__(self, largura, altura):
-        self.altura = 0
-        self.largura = 0
+        # underline para restringir o uso das váriaveis
+        self._altura = 0
+        self._largura = 0
+
+        # nome do atributo é desfigurado para evitar colisão de nomes
+        self.__var = 0
+
         self.set_altura(altura)
         self.set_largura(largura)
 
@@ -9,17 +14,18 @@ class Retangulo:
         if not (isinstance(num, int) and num > 0):
             print('a')
             raise ValueError("A altura {} é inválida".format(num))
-        self.altura = num
+        self._altura = num
+        self.__var = 456
 
     def set_largura(self, num):
         if not (isinstance(num, int) and num > 0):
             print('a')
             raise ValueError("A largura {} é inválida".format(num))
 
-        self.largura = num
+        self._largura = num
 
     def get_area(self):
-        return self.altura * self.largura
+        return self._altura * self._largura
 
 
 r = Retangulo(largura=10, altura=20)
