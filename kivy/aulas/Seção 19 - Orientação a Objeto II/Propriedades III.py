9a49eac7fca20521d7d6820b8e30c8e16d28567e
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-10 21:46:16 -0300

    Atualização

--- kivy/aulas/Seção 19 - Orientação a Objeto II/Propriedades III.py
+++ kivy/aulas/Seção 19 - Orientação a Objeto II/Propriedades III.py
@@ -1,44 +1,45 @@
 class Retangulo:
     def __init__(self, largura, altura):
-        # underline para restringir o uso das váriaveis
         self._altura = 0
         self._largura = 0
+        self.altura = altura
+        self.largura = largura
 
-        # nome do atributo é desfigurado para evitar colisão de nomes
-        self.__var = 0
-
-        self._set_altura(altura)
-        self._set_largura(largura)
-
-    def _get_altura(self):
+    @property
+    def altura(self):
         return self._altura
 
-    def _set_altura(self, num):
+    @altura.setter
+    def altura(self, num):
         if not (isinstance(num, int) and num > 0):
             print('a')
             raise ValueError("A altura {} é inválida".format(num))
         self._altura = num
         self.__var = 456
 
-    def _get_largura(self):
+    @property
+    def largura(self):
         return self._largura
 
-    def _set_largura(self, num):
+    @largura.setter
+    def largura(self, num):
         if not (isinstance(num, int) and num > 0):
             print('a')
             raise ValueError("A largura {} é inválida".format(num))
 
         self._largura = num
 
-    def get_area(self):
+    @property
+    def area(self):
         return self._altura * self._largura
 
-    altura = property(fget=_get_altura, fset=_set_altura)
-    largura = property(fget=_get_largura, fset=_set_largura)
-    area = property(fget=get_area)
+    # altura = property(fget=_get_altura, fset=_set_altura)
+    # largura = property(fget=_get_largura, fset=_set_largura)
+    # area = property(fget=get_area)
 
 
 r = Retangulo(largura=10, altura=20)
 r.largura = 10
 r.altura = 15
+# r.area = 10 => gera erro porque "area" é uma propriedade readonly
 print(r.altura, r.largura, r.area)
