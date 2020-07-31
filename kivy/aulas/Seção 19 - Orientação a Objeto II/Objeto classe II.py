a6b9a4f7a912d2286b4cd3fb1ba797d452c3387f
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-31 16:39:15 -0300

    Objeto classe II

---
+++ kivy/aulas/Seção 19 - Orientação a Objeto II/Objeto classe II.py
@@ -0,0 +1,30 @@
+# tudo declarado na classe será o corpo da classe (inclusive os métodos, diferentemente de C#)
+
+
+class MinhaClasse:
+    membro_cls = 50
+    def __init__(self):
+        self.membro_inst = -10 # membro de instância
+
+    def func(self):
+        print(self.membro_inst)
+        print(self.membro_cls)
+        print(MinhaClasse.membro_cls)
+
+
+i1 = MinhaClasse()
+i2 = MinhaClasse()
+
+print(MinhaClasse.membro_cls)
+print(i1.membro_cls)
+print(i2.membro_cls)
+
+# i1.membro_inst = 10
+# print('i1: {}'.format(i1.membro_inst))
+# print('i2: {}'.format(i2.membro_inst))
+#
+# MinhaClasse.membro_cls = 9
+#
+# print('-'*20)
+# print('i1: {}'.format(i1.membro_cls))
+# print('i2: {}'.format(i2.membro_cls))
