312744f459dc32936acb84d71a66dcc483f4c822
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-08-27 13:03:19 -0300

    Comparação entre a linguagem kivy e python - Parte 1

---
+++ kivy/aulas/Seção 24 - Kivy Language/0002 - KV vs Python - Kivy/main.py
@@ -0,0 +1,48 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+kivy.require('1.9.1')
+
+
+class Tela1(BoxLayout):
+	@staticmethod
+	def on_press_bt():
+		janela.root_window.remove_widget(janela.root)
+		janela.root_window.add_widget(Tela2())
+
+	def __init__(self, **kwargs):
+		super(Tela1, self).__init__(**kwargs)
+		self.orientation = "vertical"
+
+		bt1 = Button(text="Clique")
+		bt1.on_press = self.on_press_bt
+
+		self.add_widget(bt1)
+		self.add_widget(Button(text="bt2"))
+		self.add_widget(Button(text="bt3"))
+
+
+class Tela2(BoxLayout):
+	@staticmethod
+	def on_press_bt():
+		janela.root_window.remove_widget(janela.root)
+		janela.root_window.add_widget(Tela1())
+
+	def __init__(self, **kwargs):
+		super(Tela2, self).__init__(**kwargs)
+		self.orientation = "vertical"
+
+		bt = Button(text="Clique")
+		bt.on_press = self.on_press_bt
+		self.add_widget(bt)
+
+
+class KVvsPY2(App):
+	def build(self):
+		return Tela1()
+
+
+janela = KVvsPY2()
+janela.run()
